ff7583aab82dffc1e59e30be273f0964eb3dd1c3
Toby Cm <toby@vsus.app>
2025-03-16 15:33:34 -0700

    output info on display :fire:

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <M5Dial.h>
+#include <M5GFX.h>
 #include <Encoder.h>
 #include <USB.h>
 #include <USBHIDGamepad.h>
 
 USBHIDGamepad gamepad;
+M5GFX display;
 
 void setup()
 {
   M5Dial.begin(true);
   USB.begin();
+  display.begin();
   gamepad.begin();
   init_encoder();
 
@@ -16,21 +19,14 @@ void setup()
   Serial.println("Start");
 }
 
+int16_t lastPosition = 0;
+
 void loop()
 {
   M5Dial.update();
-  int16_t position = get_encoder();
+  int16_t position = (get_encoder() + 48) % 96;
 
-  if (position < -48)
-  {
-    position = -48;
-  }
-  if (position > 48)
-  {
-    position = 48;
-  }
-
-  int8_t joystickValue = map(position, -48, 48, -128, 127);
+  int8_t joystickValue = map(position, 0, 96, -128, 127);
 
   // Send to gamepad
   gamepad.leftStick(joystickValue, 0);
@@ -40,6 +36,17 @@ void loop()
   Serial.print(position);
   Serial.print(" -> Axis: ");
   Serial.println(joystickValue);
+
+  if (lastPosition != position)
+  {
+    lastPosition = position;
+    display.fillScreen(TFT_BLACK);
+    display.setCursor(50, 100);
+    display.printf("Position: %d\n", position);
+
+    display.setCursor(50, 120);
+    display.printf("Axis: %d\n", joystickValue);
+  }
 
   delay(10);
 }
